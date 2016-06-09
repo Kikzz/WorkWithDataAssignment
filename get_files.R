92010c95222ebe238c167d609ec2e0b0268fb988
Heikki Seppälä <heikki.seppala@siilim160205.home>
2016-06-09 15:49:11 +0300

    Initialize

---
+++ get_files.R
@@ -0,0 +1,6 @@
+
+## Create working directory if it doesn't exist
+if(!file.exists("./WorkWithDataAssignment")){dir.create("./WorkWithDataAssignment")}
+
+## set working directory
+setwd("./WorkWithDataAssignment")
