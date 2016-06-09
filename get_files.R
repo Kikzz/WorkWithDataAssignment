589b16f8d4da184a4e9e0f18e1c6dadd6164572c
Heikki Seppälä <heikki.seppala@siilim160205.home>
2016-06-09 16:09:26 +0300

    changes

--- get_files.R
+++ get_files.R
@@ -1,6 +1,18 @@
+## get_files.R
+## 1. create and set working directory
+## 2. download needed files
+## 3. 
+
+setwd("~/datasciencecoursera")
 
 ## Create working directory if it doesn't exist
 if(!file.exists("./WorkWithDataAssignment")){dir.create("./WorkWithDataAssignment")}
 
 ## set working directory
 setwd("./WorkWithDataAssignment")
+
+## download files into working directory
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",method="curl",dest="data.zip")
+
+## unzip the file. This cretates a "UCI HAR Dataset" directory and unzips all files into it.
+unzip(zipfile="data.zip")
